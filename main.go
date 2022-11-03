43f0ead73f4b1ea59094b83504e8715a4650202c
xiaoming <xiaoming_2012@outlook.com>
2022-11-03 14:04:26 +0800

    Added functions to get the url for image retrival

--- main.go
+++ main.go
@@ -3,10 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
+
+func get_date_string(time time.Time, delta int) (string, string) {
+	dt := time
+	d := fmt.Sprintf("%02d%02d%02d", dt.Year(), dt.Month(), dt.Day())
+	min := dt.Minute()
+	min = get_nearest_5min(min)
+	min = min - delta
+	t := fmt.Sprintf("%02d%02d", dt.Hour(), min)
+	return d, t
+}
 
 func get_nearest_5min(minute int) int {
 	return minute - (minute % 5)
+}
+
+func get_url_string(date string, time string) string {
+	return fmt.Sprintf("https://www.nea.gov.sg/docs/default-source/rain-area/dpsri_70km_%s%s0000dBR.dpsri.png", date, time)
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -1,14 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
-func TestHelloName(t *testing.T) {
-	ans := get_nearest_5min(1)
-	if ans != 0 {
+func TestNearest5(t *testing.T) {
+	for i := 1; i < 5; i++ {
+		ans := get_nearest_5min(i)
+		if ans != 0 {
+			t.Fatalf("failed")
+		}
+	}
+
+	for i := 21; i < 25; i++ {
+		fmt.Println("Testing ", i)
+		ans := get_nearest_5min(i)
+		if ans != 20 {
+			t.Fatalf("failed")
+		}
+	}
+}
+
+func TestURLString(t *testing.T) {
+	s := get_url_string("20221103", "0840")
+	fmt.Println(s)
+	if s != "https://www.nea.gov.sg/docs/default-source/rain-area/dpsri_70km_2022110308400000dBR.dpsri.png" {
 		t.Fatalf("failed")
+	}
+
+}
+
+func TestDateString(t *testing.T) {
+	delta := 5
+	dt := time.Date(2022, time.August, 1, 10, 11, 00, 00, time.UTC)
+	ds, ts := get_date_string(dt, delta)
+	fmt.Println(fmt.Sprintf("Date Time %s %s", ds, ts))
+
+	if ds != "20220801" || ts != "1005" {
+		t.Fatalf("Failed")
 	}
 }
